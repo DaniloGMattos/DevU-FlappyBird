05deb681e1474889fe85ade0dda4efb07afbf793
DaniloGMattos <mattosgdanilo@gmail.com>
2018-08-18 23:52:54 -0300

    [ADD] Todo o  controle do jogador , das telas e dos textos contidos nelas

---
+++ FlappyDev/FlappyDev/Assets/scripts/CountDownText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class CountDownText : MonoBehaviour {
+	public delegate void CountdownFinished();
+	public static event CountdownFinished OnCountdownFinished;
+
+	Text countDown;
+
+	void OnEnable(){
+		countDown = GetComponent<Text>();
+		countDown.text = "3";
+		StartCoroutine("Countdown");
+	}
+
+	IEnumerator Countdown() {//espera um certo tempo ate fazer algo
+		int count = 3;
+		for (int i = 0; i < count; i++){
+			countDown.text = (count - i).ToString();
+			yield return new WaitForSeconds(1);
+		}
+
+		OnCountdownFinished();
+
+	}
+
+}
---
+++ FlappyDev/FlappyDev/Assets/scripts/tapController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))] // se eu criar usar esse script em qualquer objeto, vai criar um riged body
+public class tapController : MonoBehaviour {
+public delegate void PlayerDelegate();
+public static event PlayerDelegate OnPlayerDied;
+public static event PlayerDelegate OnPlayerScored;
+
+public float tapForce = 10f;
+public float tiltSmoth = 5f;
+public Vector3 startPosition;
+Rigidbody2D rigedBody;
+Quaternion downRotation; // É uma forma diferente de rotação mais segura
+Quaternion fowardRotation;
+gameManager game;
+	// Use this for initialization
+	void Start () {
+		rigedBody = GetComponent<Rigidbody2D>();
+		downRotation = Quaternion.Euler(0,0,-90); // estou pegando um vetor 3d e convertendo em quaternion para usar na rotação e esses numeros sao os angulos 
+		fowardRotation = Quaternion.Euler(0,0,35);
+		game = gameManager.Instance;
+		
+
+		
+	}
+	void OnEnable(){
+		gameManager.OnGameStarted += OnGameStarted;
+		gameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+
+	}
+	void OnDisable(){
+		gameManager.OnGameStarted -= OnGameStarted;
+		gameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+	}
+	void OnGameStarted(){
+		rigedBody.velocity = Vector3.zero;//se eu nao botar isso o player vai ficar com toda aquela gravidade acumulada
+		rigedBody.simulated = true;
+
+	}
+	void OnGameOverConfirmed(){
+		transform.localPosition = startPosition;// se eu estiver dentro de um parent é bom usar local
+		transform.rotation = Quaternion.identity;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (game.GameOver) return;
+		if (Input.GetMouseButtonDown(0)){
+			transform.rotation = fowardRotation; //BONUS  : transform.rotation é um quaternium, nao é um vector3
+			rigedBody.velocity = Vector3.zero;
+			rigedBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+		}
+		transform.rotation = Quaternion.Lerp(transform.rotation,downRotation,tiltSmoth * Time.deltaTime);//lert vai de um source value até um target value em um certo tempo ( o tiltsmoth define o quao rápido vamos nos mover até certa ponto)
+		
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log("ok");
+		if (col.gameObject.tag == "scoreZone"){
+			//TODO: Register a score event
+			OnPlayerScored();//event sent to GameManager
+			//TODO :play sound
+
+		}
+		if (col.gameObject.tag == "deadZone"){
+			rigedBody.simulated = false;
+			//TODO : Register a dead event
+			OnPlayerDied();//event sent to GameManager
+			//TODO : play sound
+
+		}
+
+	}
+}
